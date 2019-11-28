47e7001ba5fb40011873da0a27eb24cb77cb501a
arthursrr <arthursrr@nca.ufma.br>
2019-11-28 09:18:44 -0300

    Add segmentation losses

---
+++ segmentation loss.py
@@ -0,0 +1,32 @@
+import tensorflow as tf
+from keras.losses import binary_crossentropy
+
+def dice_loss(y_true, y_pred):
+    numerator = 2 * tf.reduce_sum(y_true * y_pred, axis=(1,2,3))
+    denominator = tf.reduce_sum(y_true + y_pred, axis=(1,2,3))
+
+    return 1 - numerator / denominator
+
+def focal_loss(alpha=0.25, gamma=2):
+    def focal_loss_with_logits(logits, targets, alpha, gamma, y_pred):
+        weight_a = alpha * (1 - y_pred) ** gamma * targets
+        weight_b = (1 - alpha) * y_pred ** gamma * (1 - targets)
+        return (tf.log1p(tf.exp(-tf.abs(logits))) + tf.nn.relu(-logits)) * (weight_a + weight_b) + logits * weight_b 
+
+    def loss(y_true, y_pred):
+        y_pred = tf.clip_by_value(y_pred, tf.keras.backend.epsilon(), 1 - tf.keras.backend.epsilon())
+        logits = tf.log(y_pred / (1 - y_pred))
+
+        loss = focal_loss_with_logits(logits=logits, targets=y_true, alpha=alpha, gamma=gamma, y_pred=y_pred)
+        # or reduce_sum and/or axis=-1
+        return tf.reduce_mean(loss)
+    return loss
+
+def CrossDice_loss(y_true, y_pred):
+    def dice_loss(y_true, y_pred):
+        numerator = 2 * tf.reduce_sum(y_true * y_pred, axis=(1,2,3))
+        denominator = tf.reduce_sum(y_true + y_pred, axis=(1,2,3))
+
+        return tf.reshape(1 - numerator / denominator, (-1, 1, 1))
+
+    return binary_crossentropy(y_true, y_pred) + dice_loss(y_true, y_pred)
